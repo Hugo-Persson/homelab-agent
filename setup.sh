a10fe890caa8acf1ece3cebf009a21815eb7e718
Hugo Persson <hugo.e.persson@gmail.com>
2025-03-22 20:26:24 +0100

    Add beszel-agent service to Docker Compose and update setup script for BEZEL key management

--- setup.sh
+++ setup.sh
@@ -11,3 +11,17 @@ echo "$PASSKEY" > ./PASSKEY
 chmod 600 ./PASSKEY
 
 echo "PASSKEY has been saved securely."
+
+
+#
+echo -n "Enter your BEZEL key: "
+read -s KEY
+echo
+
+# Create bezel.env file or overwrite existing one
+echo "KEY=$KEY" > ./bezel.env
+
+# Ensure permissions are secure
+chmod 600 ./bezel.env
+
+echo "BEZEL key has been saved to bezel.env"
